878a5e918684cf38d58779147aeb7d42c968557a
AAKARSH LOHANI <aakarshlohani01@gmail.com>
2025-01-10 23:54:19 +0530

    Time Complexity: O(n)

---
+++ 06_Longest_Mountain_in_Array.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/longest-mountain-in-array/description/
+
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+from typing import List
+class Solution:
+    def longestMountain(self, arr: List[int]) -> int:
+        # Two pointer
+        N=len(arr)
+
+        base,res=0,0
+
+        while base<N:
+            end=base
+            if end+1<N and arr[end]<arr[end+1]:
+                while end+1<N and arr[end]<arr[end+1]:
+                    end+=1
+
+                # At peak                
+                if end+1<N and arr[end]>arr[end+1]:
+                    while end+1<N and arr[end]>arr[end+1]:
+                        end+=1
+
+                    res=max(end-base+1,res)
+            base=max(end,base+1)
+        return res
