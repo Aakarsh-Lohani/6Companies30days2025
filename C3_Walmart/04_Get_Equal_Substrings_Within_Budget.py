b6fa45df9b9f44cc4cdbc685ca45b21d94fa8d8c
AAKARSH LOHANI <aakarshlohani01@gmail.com>
2025-01-14 23:58:26 +0530

    Time Complexity : O(n)

---
+++ C3_Walmart/04_Get_Equal_Substrings_Within_Budget.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/get-equal-substrings-within-budget/description/
+
+# Time Complexity : O(n)
+# Space Complexity : O(1)
+class Solution:
+    def equalSubstring(self, s: str, t: str, maxCost: int) -> int:
+        maxLen=0
+
+        left,right=0,0
+        currCost=0
+        for right in range(len(s)):
+            currCost+=abs(ord(s[right])-ord(t[right]))
+
+            while currCost>maxCost:
+                currCost-=abs(ord(s[left])-ord(t[left]))
+                left+=1
+
+            maxLen=max(right-left+1,maxLen)
+
+        return maxLen
