7ff03d12d6ba7792ced905f4ea7708770157f00b
AAKARSH LOHANI <aakarshlohani01@gmail.com>
2025-01-17 00:23:46 +0530

    Time Complexity : O(n+klogn)

--- C4_Google/02_Maximum_Product_After_K_Increments.py
+++ C4_Google/02_Maximum_Product_After_K_Increments.py
@@ -1,2 +1,21 @@
 # https://leetcode.com/problems/maximum-product-after-k-increments/description/
 
+# Time Complexity : O(n+klogn)
+# Space Complexity : O(n)
+import heapq
+from typing import List
+class Solution:
+    def maximumProduct(self, nums: List[int], k: int) -> int:
+        # Need to increment minimum number in list at each operation
+        prod=1
+        min_heap=nums.copy()  
+        heapq.heapify(min_heap)  # O(n)
+
+        for i in range(k):  #O(klogn)
+            num=heapq.heappop(min_heap)
+            heapq.heappush(min_heap,num+1)  
+
+        for n in min_heap:
+            prod=(prod*n)%(10**9+7)
+
+        return prod
