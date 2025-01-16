5f13313c55676449111434568e62be26265c8e40
AAKARSH LOHANI <aakarshlohani01@gmail.com>
2025-01-17 00:53:08 +0530

    Time Complexity : O(n+klogn)

--- C4_Google/02_Maximum_Product_After_K_Increments.py
+++ C4_Google/02_Maximum_Product_After_K_Increments.py
@@ -8,13 +8,13 @@ class Solution:
     def maximumProduct(self, nums: List[int], k: int) -> int:
         # Need to increment minimum number in list at each operation
         prod=1
-        min_heap=nums.copy()  
-        heapq.heapify(min_heap)  # O(n)
+        min_heap=nums.copy()
+        heapq.heapify(min_heap)
 
-        for i in range(k):  #O(klogn)
-            num=heapq.heappop(min_heap)
-            heapq.heappush(min_heap,num+1)  
-
+        for i in range(k):
+            num=min_heap[0]
+            heapq.heapreplace(min_heap,num+1)  # More efficient than pop followed by push
+ 
         for n in min_heap:
             prod=(prod*n)%(10**9+7)
 
