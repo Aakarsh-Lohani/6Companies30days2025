8d93ac4e493bb1a87baaa34cced35d8cdb6cc86b
AAKARSH LOHANI <aakarshlohani01@gmail.com>
2025-01-17 23:59:38 +0530

    Time Complexity: O(n⋅logn+m⋅logm)

---
+++ C5_Atlassian/01_Assign_Cookies.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/assign-cookies/description/
+
+
+# Time Complexity: O(n⋅logn+m⋅logm)
+# Space Complexity: O(m+n) # Tim sort in Python
+from typing import List
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+        satisfiedChildren,CurrCookieId=0,0
+
+        while satisfiedChildren<len(g) and CurrCookieId<len(s):
+            if s[CurrCookieId]>=g[satisfiedChildren]:
+                satisfiedChildren+=1
+            CurrCookieId+=1
+            
+
+        return satisfiedChildren
